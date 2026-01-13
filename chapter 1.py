15d34c1f200a4e952378df603829f1ef6679e77d
Chetan Urf Ishant <ishunara25@gmail.com>
2026-01-13 20:33:20 +0530

    chapter 1.py

--- chapter 1.py
+++ chapter 1.py
@@ -1,2 +1,24 @@
 #this is the very first chapter of python 
 #track daily codes in this repo accordingly.
+
+"""
+Python is :
+open source
+object oriented language
+high-level language
+free to use
+interpreted(runs code line-by-line)
+"""
+
+# Python is widely used because it is easy to understand and writing code in python
+# is far easy than writing code in other programming languages 
+
+# Modules in python : 
+# it is a file containing code written by somebody else(usually) which can be imported and used in our programs.
+
+# PIP: it is used to install module on our system
+
+#first python program:
+print("Hello GitHub Family")
+
+#this repo is majorly focused to crack technical interviews
