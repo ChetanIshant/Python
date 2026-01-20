cf897b4dcbf497a0e270066a3e36300a30641ace
Chetan Urf Ishant <ishunara25@gmail.com>
2026-01-20 23:36:20 +0530

    Add files via upload

---
+++ 05_lists.py
@@ -0,0 +1,9 @@
+#list is a mutable data type in python
+#it is started and ended with square brackets []
+my_list = [1, 2, 3, 4, 5]
+print(my_list)
+#list can have different data types
+my_list2 = [1, "hello", 3.5, True]
+print(my_list2)
+my_list2.append("new item") #to add an item at the end of the list  
+print(my_list2)
