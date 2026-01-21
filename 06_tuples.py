f09bd31ccecf9e2a1aee27ac975808b08be89daa
Chetan Urf Ishant <ishunara25@gmail.com>
2026-01-21 23:05:14 +0530

    Add files via upload

---
+++ 06_tuples.py
@@ -0,0 +1,13 @@
+#tuples are immutable data types in python
+#it is started and ended with parentheses ()
+a = (1, 2, 3, 4, 5)
+print(a)
+#tuple can have different data types
+my_tuple2 = (1, "hello", 3.5, True)
+print(my_tuple2)
+
+#methods of tuple
+no=a.count(2) #to count the number of occurrences of an item in the tuple
+print(no)
+index=a.index(3) #to find the index of an item in the tuple
+print(index)
