ed825052a5216959e43a4f1795b5b53a46eaa156
Chetan Urf Ishant <ishunara25@gmail.com>
2026-02-21 12:31:42 +0530

    Update 05_lists.py

--- 05_lists.py
+++ 05_lists.py
@@ -6,4 +6,10 @@ print(my_list)
 my_list2 = [1, "hello", 3.5, True]
 print(my_list2)
 my_list2.append("new item") #to add an item at the end of the list  
+
 print(my_list2)
+
+#Lists in Python are ordered and mutable data structures used to store multiple items in a single variable. 
+#They are defined using square brackets [] and can contain elements of different data types, such as integers, strings, floats, or even other lists. 
+#Lists support indexing, slicing, and many built-in methods like append(), remove(), and sort(). Because they are mutable, 
+#their elements can be modified after creation, making them highly flexible.
